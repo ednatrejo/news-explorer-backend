32c464a0e12d683a0868da2b1515f1cb1db97275
ednatrejo <Trejo.edna1379@gmail.com>
2024-07-16 13:13:41 -0500

    fixing errora

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    commonjs: true,
-  },
-  extends: ["eslint:recommended", "airbnb-base", "prettier"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "no-console": "off",
-    "no-underscore-dangle": ["error", { allow: ["_id"] }],
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
